98a661a9b449734773bf3de263bed3317bec9fbd
quyetnguyen22 <you@example.comquyetnguyen15na@gmail.com>
2022-07-05 08:28:16 +0700

    IntroductionToJavaArray git commit -mArray git commit -mIntroductionToJavaArray

--- MODULE_2/src/SS3_Array_And_Method/Exercise/AddElement.java
+++ MODULE_2/src/SS3_Array_And_Method/Exercise/AddElement.java
@@ -3,9 +3,8 @@ package SS3_Array_And_Method.Exercise;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Erase_Element {
+public class AddElement {
     public static void main(String[] args) {
-//        int[] arr = {10, 4, 6, 7, 8, 6, 0, 0, 0, 0};
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array.");
         int aNumberOf = scanner.nextInt();
@@ -17,21 +16,23 @@ public class Erase_Element {
         }
         System.out.println("The array\n" + Arrays.toString(arr));
         Scanner scanner2 = new Scanner(System.in);
-        System.out.println("Input the element that you want to erase.");
+        System.out.println("Input the number that you want to add.");
         int number = scanner2.nextInt();
-        boolean isNotExist = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == number) {
-                for (int j = i; j < aNumberOf - 1; j++) {
-                    arr[j] = arr[j + 1];
-                    isNotExist = false;
-                }
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Input the position that you want to add.");
+        int index = scanner3.nextInt();
+        int[] newArr = new int[aNumberOf];
+        if (index < 0 || index > arr.length) {
+            System.out.println("Invalid!");
+        } else {
+            for (int i = 0; i < index; i++) {
+                newArr[i] = arr[i];
+            }
+            newArr[index] = number;
+            for (int i = index + 1; i < newArr.length; i++) {
+                newArr[i] = arr[i - 1];
             }
         }
-        System.out.print(Arrays.toString(arr) + "\t");
-        if (isNotExist) {
-            System.out.println("\nThe number which you entered is not exist.");
-
-        }
+        System.out.println(Arrays.toString(newArr));
     }
 }
